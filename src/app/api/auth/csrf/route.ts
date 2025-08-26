f134a60c139d4f26dcae5f63f6c6c5a6760fa8fa
Aiyatullah <114688719+Aiyatullah@users.noreply.github.com>
2025-08-26 17:34:39 +0530

    Update route.ts

--- src/app/api/auth/csrf/route.ts
+++ src/app/api/auth/csrf/route.ts
@@ -1,10 +1,8 @@
-import {auth} from "@/../lib/auth";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const { csrfToken } = await auth.getCsrfToken();
-    return NextResponse.json({ csrfToken });
+     console.log("hello")
   } catch (error) {
     return NextResponse.json(
       { error: "Error generating CSRF token" },
